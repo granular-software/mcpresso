2b027afb04ba623beeea39db05efc63538be5d71
Arthur Hirel <arthurhirel@gmail.com>
2025-06-20 18:16:58 +0200

    Stripe example

--- examples/mcpresso.ts
+++ examples/mcpresso.ts
@@ -36,9 +36,13 @@ const tagResource = createResource({
 	name: "tag",
 	schema: TagSchema,
 	uri_template: "tags/{id}",
-	handlers: {
-		get: async ({ id }) => tags.find((tag) => tag.id === id),
-		list: async () => tags,
+	methods: {
+		get: {
+			handler: async ({ id }) => tags.find((tag) => tag.id === id),
+		},
+		list: {
+			handler: async () => tags,
+		},
 	},
 });
 
@@ -51,85 +55,94 @@ const noteResource = createResource({
 		authorId: { type: "user" },
 		tagIds: { type: "tag" },
 	},
-	methodConfig: {
+	methods: {
+		get: {
+			handler: async ({ id }) => {
+				console.log(`Getting note with id: ${id}`);
+				return notes.find((note) => note.id === id);
+			},
+		},
+		create: {
+			handler: async (data) => {
+				console.log("Creating a new note with data:", data);
+
+				if (!data.authorId) {
+					throw new Error("Missing authorId");
+				}
+				const authorExists = users.some((user) => user.id === data.authorId);
+				if (!authorExists) {
+					throw new Error(`Author with id '${data.authorId}' not found.`);
+				}
+
+				if (!data.tagIds) {
+					throw new Error("Missing tagIds");
+				}
+				for (const tagId of data.tagIds) {
+					if (!tags.some((tag) => tag.id === tagId)) {
+						throw new Error(`Tag with id '${tagId}' not found.`);
+					}
+				}
+
+				const newNote = {
+					id: (notes.length + 1).toString(),
+					createdAt: new Date(),
+					content: data.content ?? "",
+					authorId: data.authorId,
+					tagIds: data.tagIds,
+				};
+
+				notes.push(newNote);
+				return newNote;
+			},
+		},
 		update: {
-			pick: ["content", "tagIds"],
+			// Override the default input schema to only allow picking certain fields
+			// inputSchema: NoteSchema.pick({ content: true, tagIds: true }).partial().extend({ id: z.string() }),
+			
+            handler: async ({ id, ...data }) => {
+				console.log(`Updating note with id: ${id} with data:`, data);
+				const index = notes.findIndex((note) => note.id === id);
+				if (index === -1) {
+					return null as any; // MCP server will return null
+				}
+				notes[index] = { ...notes[index], ...data };
+				return notes[index];
+			},
+		},
+		delete: {
+			handler: async ({ id }) => {
+				console.log(`Deleting note with id: ${id}`);
+				const index = notes.findIndex((note) => note.id === id);
+				if (index === -1) {
+					return { success: false };
+				}
+				notes.splice(index, 1);
+				return { success: true };
+			},
+		},
+		list: {
+			handler: async () => {
+				console.log("Listing all notes");
+				return notes;
+			},
 		},
 		search: {
-			schema: z.object({
+			description: "Search for notes by a query string.",
+			inputSchema: z.object({
 				query: z.string().describe("The text to search for in the note content."),
 			}),
+			handler: async ({ query }) => {
+				console.log(`Searching for notes with query: "${query}"`);
+				return notes.filter((note) => note.content.includes(query));
+			},
 		},
-	},
-	handlers: {
-		get: async ({ id }) => {
-			console.log(`Getting note with id: ${id}`);
-			return notes.find((note) => note.id === id);
-		},
-		create: async (data) => {
-			console.log("Creating a new note with data:", data);
-
-			if (!data.authorId) {
-				throw new Error("Missing authorId");
-			}
-			const authorExists = users.some((user) => user.id === data.authorId);
-			if (!authorExists) {
-				throw new Error(`Author with id '${data.authorId}' not found.`);
-			}
-
-			if (!data.tagIds) {
-				throw new Error("Missing tagIds");
-			}
-			for (const tagId of data.tagIds) {
-				if (!tags.some((tag) => tag.id === tagId)) {
-					throw new Error(`Tag with id '${tagId}' not found.`);
-				}
-			}
-
-			const newNote = {
-				id: (notes.length + 1).toString(),
-				createdAt: new Date(),
-				content: data.content ?? "",
-				authorId: data.authorId,
-				tagIds: data.tagIds,
-			};
-
-			notes.push(newNote);
-			return newNote;
-		},
-		update: async ({ id, ...data }) => {
-			console.log(`Updating note with id: ${id} with data:`, data);
-			const index = notes.findIndex((note) => note.id === id);
-			if (index === -1) {
-				return undefined;
-			}
-			notes[index] = { ...notes[index], ...data };
-			return notes[index];
-		},
-		delete: async ({ id }) => {
-			console.log(`Deleting note with id: ${id}`);
-			const index = notes.findIndex((note) => note.id === id);
-			if (index === -1) {
-				return false;
-			}
-			notes.splice(index, 1);
-			return true;
-		},
-		list: async () => {
-			console.log("Listing all notes");
-			return notes;
-		},
-		search: async ({ query }) => {
-			console.log(`Searching for notes with query: "${query}"`);
-			return notes.filter((note) => note.content.includes(query));
-		},
-	},
-
-	customMethods: {
 		count_by_author: {
 			description: "Counts how many notes an author has written.",
 			inputSchema: z.object({
 				authorId: z.string().describe("The ID of the author."),
+			}),
+			outputSchema: z.object({
+				count: z.number(),
 			}),
 			handler: async ({ authorId }) => {
 				const count = notes.filter((note) => note.authorId === authorId).length;
@@ -144,28 +157,39 @@ const userResource = createResource({
 	name: "user",
 	schema: UserSchema,
 	uri_template: "users/{id}",
-	handlers: {
-		get: async ({ id }) => {
-			return users.find((user) => user.id === id);
+	methods: {
+		get: {
+			handler: async ({ id }) => {
+				return users.find((user) => user.id === id);
+			},
 		},
-		list: async () => {
-			return users;
+		list: {
+			handler: async () => {
+				return users;
+			},
+		},
+		update: {
+			handler: async ({ id, ...data }) => {
+				const index = users.findIndex((user) => user.id === id);
+				if (index === -1) {
+					return null as any;
+				}
+			},
 		},
 	},
 });
 
 // Create the MCP server
 const server = createMCPServer({
-	name: "my_server",
+	name: "my_serv",
 	serverUrl: "http://localhost:3080",
 	resources: [noteResource, userResource, tagResource],
 	exposeTypes: [noteResource, userResource, tagResource],
 	// auth: {
-	//   issuer: 'https://your-auth-server.com', // e.g., your Auth0 or Okta issuer URL
+	//   issuer: 'https://your-auth-server.com',
 	// },
 });
 
-// Start the server
 server.listen(3080, () => {
 	console.log("MCPresso server running on http://localhost:3080");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,204 +1,204 @@
-import express, { text, type Request, type Response } from "express";
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import { z } from "zod";
-import { type CallToolResult, type GetPromptResult, type ReadResourceResult } from "@modelcontextprotocol/sdk/types.js";
+// import express, { text, type Request, type Response } from "express";
+// import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+// import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+// import { z } from "zod";
+// import { type CallToolResult, type GetPromptResult, type ReadResourceResult } from "@modelcontextprotocol/sdk/types.js";
 
-const getServer = () => {
-	// Create an MCP server with implementation details
-	const server = new McpServer(
-		{
-			name: "stateless-streamable-http-server",
-			version: "1.0.0",
-		},
-		{ capabilities: { logging: {} } },
-	);
+// const getServer = () => {
+// 	// Create an MCP server with implementation details
+// 	const server = new McpServer(
+// 		{
+// 			name: "stateless-streamable-http-server",
+// 			version: "1.0.0",
+// 		},
+// 		{ capabilities: { logging: {} } },
+// 	);
 
-	// Register a simple prompt
-	server.prompt(
-		"greeting-template",
-		"A simple greeting prompt template",
-		{
-			firstname: z.string().describe("First name to include in greeting"),
-			lastname: z.string().describe("Last name to include in greeting"),
-		},
-		async ({ firstname, lastname }): Promise<GetPromptResult> => {
-			return {
-				messages: [
-					{
-						role: "user",
-						content: {
-							type: "text",
-							text: `Please greet ${firstname} ${lastname} in a friendly manner.`,
-						},
-					},
-				],
-			};
-		},
-	);
+// 	// Register a simple prompt
+// 	server.prompt(
+// 		"greeting-template",
+// 		"A simple greeting prompt template",
+// 		{
+// 			firstname: z.string().describe("First name to include in greeting"),
+// 			lastname: z.string().describe("Last name to include in greeting"),
+// 		},
+// 		async ({ firstname, lastname }): Promise<GetPromptResult> => {
+// 			return {
+// 				messages: [
+// 					{
+// 						role: "user",
+// 						content: {
+// 							type: "text",
+// 							text: `Please greet ${firstname} ${lastname} in a friendly manner.`,
+// 						},
+// 					},
+// 				],
+// 			};
+// 		},
+// 	);
 
-	// Register a tool specifically for testing resumability
-	server.tool(
-		"start-notification-stream",
-		"Starts sending periodic notifications for testing resumability",
-		{
-			interval: z.number().describe("Interval in milliseconds between notifications").default(100),
-			count: z.number().describe("Number of notifications to send (0 for 100)").default(10),
-		},
-		async ({ interval, count }, { sendNotification, requestId, authInfo }): Promise<CallToolResult> => {
-			const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-			let counter = 0;
-			console.log("Request ID:", requestId);
-			console.log("Auth Info:", authInfo);
+// 	// Register a tool specifically for testing resumability
+// 	server.tool(
+// 		"start-notification-stream",
+// 		"Starts sending periodic notifications for testing resumability",
+// 		{
+// 			interval: z.number().describe("Interval in milliseconds between notifications").default(100),
+// 			count: z.number().describe("Number of notifications to send (0 for 100)").default(10),
+// 		},
+// 		async ({ interval, count }, { sendNotification, requestId, authInfo }): Promise<CallToolResult> => {
+// 			const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// 			let counter = 0;
+// 			console.log("Request ID:", requestId);
+// 			console.log("Auth Info:", authInfo);
 
-			while (count === 0 || counter < count) {
-				counter++;
-				try {
-					await sendNotification({
-						method: "notifications/message",
-						params: {
-							level: "info",
-							data: `Periodic notification #${counter} at ${new Date().toISOString()}`,
-						},
-					});
-				} catch (error) {
-					console.error("Error sending notification:", error);
-				}
-				// Wait for the specified interval
-				await sleep(interval);
-			}
+// 			while (count === 0 || counter < count) {
+// 				counter++;
+// 				try {
+// 					await sendNotification({
+// 						method: "notifications/message",
+// 						params: {
+// 							level: "info",
+// 							data: `Periodic notification #${counter} at ${new Date().toISOString()}`,
+// 						},
+// 					});
+// 				} catch (error) {
+// 					console.error("Error sending notification:", error);
+// 				}
+// 				// Wait for the specified interval
+// 				await sleep(interval);
+// 			}
 
-			return {
-				content: [
-					{
-						type: "text",
-						text: `Started sending periodic notifications every ${interval}ms`,
-					},
-				],
-			};
-		},
-	);
+// 			return {
+// 				content: [
+// 					{
+// 						type: "text",
+// 						text: `Started sending periodic notifications every ${interval}ms`,
+// 					},
+// 				],
+// 			};
+// 		},
+// 	);
 
-	server.tool("add", "Adds two numbers", { a: z.number().describe("The first number"), b: z.number().describe("The second number") }, async ({ a, b }) => ({
-		content: [{ type: "text", text: String(a + b) }],
-	}));
+// 	server.tool("add", "Adds two numbers", { a: z.number().describe("The first number"), b: z.number().describe("The second number") }, async ({ a, b }) => ({
+// 		content: [{ type: "text", text: String(a + b) }],
+// 	}));
 
-	server.resource("echo", "https://example.com/echolisr-", { mimeType: "text/plain" }, async (uri) => ({
-		contents: [
-			{
-				uri: uri.href,
-				text: "hello!",
-			},
-		],
-	}));
+// 	server.resource("echo", "https://example.com/echolisr-", { mimeType: "text/plain" }, async (uri) => ({
+// 		contents: [
+// 			{
+// 				uri: uri.href,
+// 				text: "hello!",
+// 			},
+// 		],
+// 	}));
 
-	server.resource("greeting", new ResourceTemplate("https://example.com/greet/{name}", { list: undefined }), async (uri, { name }) => ({
-		contents: [
-			{
-				uri: uri.href,
-				text: `Hello, ${name}!`,
-			},
-		],
-	}));
+// 	server.resource("greeting", new ResourceTemplate("https://example.com/greet/{name}", { list: undefined }), async (uri, { name }) => ({
+// 		contents: [
+// 			{
+// 				uri: uri.href,
+// 				text: `Hello, ${name}!`,
+// 			},
+// 		],
+// 	}));
 
-	server.resource(
-		"type",
-		"type://granular.software/user",
-		{
-			mimeType: "application/json",
-		},
-		async (uri) => ({
-			contents: [
-				{
-					uri: uri.href,
-					mimeType: "application/json",
-					text: JSON.stringify({
-						name: "John Doe",
-						age: 30,
-					}),
-				},
-			],
-		}),
-	);
+// 	server.resource(
+// 		"type",
+// 		"type://granular.software/user",
+// 		{
+// 			mimeType: "application/json",
+// 		},
+// 		async (uri) => ({
+// 			contents: [
+// 				{
+// 					uri: uri.href,
+// 					mimeType: "application/json",
+// 					text: JSON.stringify({
+// 						name: "John Doe",
+// 						age: 30,
+// 					}),
+// 				},
+// 			],
+// 		}),
+// 	);
 
-	return server;
-};
+// 	return server;
+// };
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const server = getServer();
+// const server = getServer();
 
-setTimeout(() => {
-	server.tool("multiply", "Multiplies two numbers", { a: z.number().describe("The first number"), b: z.number().describe("The second number") }, async ({ a, b }) => ({
-		content: [{ type: "text", text: String(a * b) }],
-	}));
-	console.log("Added multiply tool");
-}, 15000);
+// setTimeout(() => {
+// 	server.tool("multiply", "Multiplies two numbers", { a: z.number().describe("The first number"), b: z.number().describe("The second number") }, async ({ a, b }) => ({
+// 		content: [{ type: "text", text: String(a * b) }],
+// 	}));
+// 	console.log("Added multiply tool");
+// }, 15000);
 
-app.post("/mcp", async (req: Request, res: Response) => {
-	try {
-		const transport: StreamableHTTPServerTransport = new StreamableHTTPServerTransport({
-			sessionIdGenerator: undefined,
-		});
-		await server.connect(transport);
-		await transport.handleRequest(req, res, req.body);
-		res.on("close", () => {
-			console.log("Request closed");
-			transport.close();
-			server.close();
-		});
-	} catch (error) {
-		console.error("Error handling MCP request:", error);
-		if (!res.headersSent) {
-			res.status(500).json({
-				jsonrpc: "2.0",
-				error: {
-					code: -32603,
-					message: "Internal server error",
-				},
-				id: null,
-			});
-		}
-	}
-});
+// app.post("/mcp", async (req: Request, res: Response) => {
+// 	try {
+// 		const transport: StreamableHTTPServerTransport = new StreamableHTTPServerTransport({
+// 			sessionIdGenerator: undefined,
+// 		});
+// 		await server.connect(transport);
+// 		await transport.handleRequest(req, res, req.body);
+// 		res.on("close", () => {
+// 			console.log("Request closed");
+// 			transport.close();
+// 			server.close();
+// 		});
+// 	} catch (error) {
+// 		console.error("Error handling MCP request:", error);
+// 		if (!res.headersSent) {
+// 			res.status(500).json({
+// 				jsonrpc: "2.0",
+// 				error: {
+// 					code: -32603,
+// 					message: "Internal server error",
+// 				},
+// 				id: null,
+// 			});
+// 		}
+// 	}
+// });
 
-app.get("/mcp", async (req: Request, res: Response) => {
-	console.log("Received GET MCP request");
-	res.writeHead(405).end(
-		JSON.stringify({
-			jsonrpc: "2.0",
-			error: {
-				code: -32000,
-				message: "Method not allowed.",
-			},
-			id: null,
-		}),
-	);
-});
+// app.get("/mcp", async (req: Request, res: Response) => {
+// 	console.log("Received GET MCP request");
+// 	res.writeHead(405).end(
+// 		JSON.stringify({
+// 			jsonrpc: "2.0",
+// 			error: {
+// 				code: -32000,
+// 				message: "Method not allowed.",
+// 			},
+// 			id: null,
+// 		}),
+// 	);
+// });
 
-app.delete("/mcp", async (req: Request, res: Response) => {
-	console.log("Received DELETE MCP request");
-	res.writeHead(405).end(
-		JSON.stringify({
-			jsonrpc: "2.0",
-			error: {
-				code: -32000,
-				message: "Method not allowed.",
-			},
-			id: null,
-		}),
-	);
-});
+// app.delete("/mcp", async (req: Request, res: Response) => {
+// 	console.log("Received DELETE MCP request");
+// 	res.writeHead(405).end(
+// 		JSON.stringify({
+// 			jsonrpc: "2.0",
+// 			error: {
+// 				code: -32000,
+// 				message: "Method not allowed.",
+// 			},
+// 			id: null,
+// 		}),
+// 	);
+// });
 
-// Start the server
-const PORT = 4000;
-app.listen(PORT, () => {
-	console.log(`MCP Stateless Streamable HTTP Server listening on port ${PORT}`);
-});
+// // Start the server
+// const PORT = 4000;
+// app.listen(PORT, () => {
+// 	console.log(`MCP Stateless Streamable HTTP Server listening on port ${PORT}`);
+// });
 
-// Handle server shutdown
-process.on("SIGINT", async () => {
-	console.log("Shutting down server...");
-	process.exit(0);
-});
+// // Handle server shutdown
+// process.on("SIGINT", async () => {
+// 	console.log("Shutting down server...");
+// 	process.exit(0);
+// });
